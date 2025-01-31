2fe41e540b3e16d7f6bd74f0d5b948e455540be2
Matheus Rosmaninho <matheusrosmano@gmail.com>
2025-01-30 21:46:57 -0300

    feat: Cria infra do cloudflare

---
+++ cloudflare/opentofu/variables.tf
@@ -0,0 +1,17 @@
+variable "cloudflareApiToken" {
+  type = string
+  sensitive = true
+}
+
+variable "remoteStateBucketName" {
+  type = string
+}
+
+variable "remoteStateBucketRegion" {
+  type = string
+}
+
+variable "zoneIdTerminalbaka" {
+  type = string
+  sensitive = true
+}
